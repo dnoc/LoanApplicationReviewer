26450d17e9a2d16f05b88d1252a562096f03df21
dnoc <drew.noct@gmail.com>
2022-02-07 17:00:12 -0500

    calc methods for application

--- app/models/application.rb
+++ app/models/application.rb
@@ -10,9 +10,23 @@ class Application < ActiveRecord::Base
   attr_accessor :provided_id
 
   validates_presence_of :provided_id
-  validates :borrowers, length: { maximum: 2 }
+  validates :borrowers, length: { within: 1..2 }
+  validates :loans, length: { minimum: 1 }
 
   def initialize(provided_id:)
     @provided_id = provided_id
+  end
+
+  def credit_score
+    borrowers.min_by { |b| b.credit_score }
+  end
+
+  def dti
+    total_loan_payments = loans.sum { |loan| loan.monthly_payment }
+    applicable_liabilities = liabilities.filter { |l| l.applicable_to_dti? }
+
+    total_applicable_liability = total_loan_payments + applicable_liabilities.sum { |l| l.monthly_payment }
+
+    total_applicable_liability / incomes.sum { |i| i.monthly_amount }
   end
 end
